789e6cb2d94fff0da4eb4f157e0f833e4437b6af
Basma Hawash <125481348+basmasalim@users.noreply.github.com>
2024-01-15 08:55:08 +0200

    Update assignment-07.js

--- [048-053] Loop - For/assignment-07.js
+++ [048-053] Loop - For/assignment-07.js
@@ -3,7 +3,7 @@ let mix = [1, 2, 3, "A", "B", "C", 4];
 
 for (let i = start; i < mix.length; i++) {
   if (typeof mix[i] === "number") {
-    if (mix[i] === 1) {
+    if (mix[i] === (true + start) {
       continue;
     }
     console.log(mix[i]);
